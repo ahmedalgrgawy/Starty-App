9953d61d1c2c22d5583349350386109cb86acda1
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-12 18:12:09 +0200

    Define Auth User

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import StartUpCard, { StartupTypeCard } from "@/components/StartUpCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query: string }>;
@@ -12,6 +13,11 @@ export default async function Home({ searchParams }: {
     const params = { search: query || null }
 
     const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
+
+    const session = await auth()
+
+    console.log(session?.id);
+
 
     return (
         <>
--- components/View.tsx
+++ components/View.tsx
@@ -1,12 +1,20 @@
 import { client } from "@/sanity/lib/client"
 import Ping from "./Ping"
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries"
+import { writeClient } from "@/sanity/lib/write-client"
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
 
     const { views } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id })
 
-    // update view count
+    after(
+        async () =>
+            await writeClient
+                .patch(id)
+                .set({ views: views + 1 })
+                .commit(),
+    );
 
     return (
         <div className="view-container">
